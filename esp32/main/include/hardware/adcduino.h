ebbe04f4962d669539ac8e29c7071ec28a1cfcf8
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2023-04-16 19:12:34 -0400

    inlining

--- esp32/main/include/hardware/adcduino.h
+++ esp32/main/include/hardware/adcduino.h
@@ -2,12 +2,15 @@
 #define ADCDUINO_H
 
 #include </Users/willsturgeon/esp/esp-idf/components/driver/deprecated/driver/adc.h>
+#include <driver/rtc_io.h>
+#include <esp_log.h>
 #include <hal/adc_ll.h>
 #include <hal/gpio_hal.h>
 #include <soc/adc_channel.h>
 #include <soc/rtc_io_reg.h>
 #include <soc/sens_reg.h>
 
+#include <inttypes.h>
 #include <stdint.h>
 
 #define ADC_ATTENUATION 3
@@ -52,7 +55,49 @@ __attribute__((always_inline)) inline static void dropin_gpio_config(void) {
       .pull_down_en = GPIO_PULLDOWN_DISABLE, /*!< GPIO pull-down                                       */
       .intr_type = GPIO_INTR_DISABLE,
   };
-  ESP_ERROR_CHECK(gpio_config(&GPIO_CONF));
+  // ESP_ERROR_CHECK(gpio_config(&GPIO_CONF));
+
+  uint32_t io_reg = 0;
+  uint32_t io_num = 0;
+  uint8_t input_en = 0;
+  uint8_t output_en = 0;
+  uint8_t od_en = 0;
+  uint8_t pu_en = 0;
+  uint8_t pd_en = 0;
+
+  do {
+    io_reg = GPIO_PIN_MUX_REG[io_num];
+
+    if ((((1ULL << 36) >> io_num) & BIT(0))) {
+      assert(io_reg != (intptr_t)NULL);
+
+      if (rtc_gpio_is_valid_gpio(io_num)) {
+        rtc_gpio_deinit(io_num);
+      }
+
+      ESP_LOGI("gpio", "GPIO[%" PRIu32 "]| InputEn: %d| OutputEn: %d| OpenDrain: %d| Pullup: %d| Pulldown: %d| Intr:%d ", io_num, input_en, output_en, od_en, pu_en, pd_en, GPIO_CONF.intr_type);
+      gpio_set_intr_type(io_num, GPIO_CONF.intr_type);
+
+      if (GPIO_CONF.intr_type) {
+        gpio_intr_enable(io_num);
+      } else {
+        gpio_intr_disable(io_num);
+      }
+
+#if SOC_GPIO_SUPPORT_PIN_HYS_FILTER
+      printf("HYSTERESIS FILTER SUPPORTED\r\n");
+      if (GPIO_CONF.hys_ctrl_mode == GPIO_HYS_SOFT_ENABLE) {
+        gpio_hysteresis_enable(io_num);
+      } else if (GPIO_CONF.hys_ctrl_mode == GPIO_HYS_SOFT_DISABLE) {
+        gpio_hysteresis_disable(io_num);
+      } else {
+        gpio_hysteresis_by_efuse(io_num);
+      }
+#endif // SOC_GPIO_SUPPORT_PIN_HYS_FILTER
+      /* By default, all the pins have to be configured as GPIO pins. */
+      gpio_hal_iomux_func_sel(io_reg, PIN_FUNC_GPIO);
+    }
+  } while (++io_num < GPIO_PIN_COUNT);
 }
 
 __attribute__((always_inline)) inline static void dropin_adc1_config_channel_atten(void) {
